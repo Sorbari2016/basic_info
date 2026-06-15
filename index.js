9f669438e2586e98e2d5bd3aa95f522a8baf42f6
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-06-15 15:53:57 +0100

    Update server to express

--- index.js
+++ index.js
@@ -1,77 +1,44 @@
-// Use in-built node modules
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+// Import packages
+import express from "express";
+import path from "path";
 
-// Create port
-const PORT = 8080;
+const app = express(); // initial an express instance
 
-// Create HTTP server instance
-const server = http.createServer((req, res) => {
-  // For static assets
-  if (req.url.startsWith("/assets/")) {
-    // for file name with space between, decode %20 back into normal space
-    const decodeURL = decodeURIComponent(req.url);
-    const filePath = path.join(__dirname, decodeURL);
+// Use a middleware to access static files
+app.use("/public", express.static(path.join(import.meta.dirname, "public")));
 
-    fs.readFile(filePath, (err, content) => {
-      if (err) {
-        res.statusCode = 404;
-        res.end("File not found");
-        return;
-      }
-
-      const ext = path.extname(filePath); // grab file extension
-
-      const mimeTypes = {
-        ".css": "text/css",
-        ".png": "image/png",
-        ".jpg": "image/jpeg",
-        ".jpeg": "image/jpeg",
-        ".gif": "image/gif",
-        ".svg": "image/svg+xml",
-      }; // a lookup for the browser
-
-      res.setHeader(
-        "Content-Type",
-        mimeTypes[ext] || "application/octet-stream",
-      );
-
-      res.end(content);
-    });
-
-    return;
-  }
-  // initialize file path
-  let filePath;
-
-  // update file path
-  if (req.url === "/") {
-    filePath = path.join(__dirname, "index.html");
-  } else if (req.url === "/about") {
-    filePath = path.join("pages", "about.html");
-  } else if (req.url === "/contact-us") {
-    filePath = path.join("pages", "contact-us.html");
-  } else {
-    filePath = path.join("pages", "404.html");
-    res.statusCode = 404;
-  }
-
-  // Read and send content
-  fs.readFile(filePath, (err, content) => {
-    // handle error
-    if (err) {
-      res.statusCode = 500;
-      res.end("Internal server error");
-      return;
-    }
-    // respond with content
-    res.setHeader("content-type", "text/html");
-    res.end(content);
-  });
+// Define GET route to get homepage
+app.get("/", (req, res) => {
+  const filePath = path.join(import.meta.dirname, "index.html"); // create absolute path
+  res.sendFile(filePath);
 });
 
-// Bind the server to your designated port
-server.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+// Define GET route for about page
+app.get("/about", (req, res) => {
+  const filePath = path.join(import.meta.dirname, "pages/about.html");
+  res.sendFile(filePath);
+});
+
+// Define GET route for contact us page
+app.get("/contact-us", (req, res) => {
+  const filePath = path.join(import.meta.dirname, "pages/contact-us.html");
+  res.sendFile(filePath);
+});
+
+// Handle unavailable resource
+app.use((req, res, next) => {
+  res.status(404);
+  const filePath = path.join(import.meta.dirname, "pages/404.html");
+  res.sendFile(filePath);
+});
+
+// Define port
+const PORT = 8080;
+
+// Start an express server
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Server is running on Port ${PORT}`);
 });
