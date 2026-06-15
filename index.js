ca340178ccb4a35323cec3c36304b9979980d55f
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-06-15 12:39:27 +0100

    Add static images to hp, rewrote server app to render images

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ const PORT = 8080;
 
 // Create HTTP server instance
 const server = http.createServer((req, res) => {
+  // For static assets
+  if (req.url.startsWith("/assets/")) {
+    // for file name with space between, decode %20 back into normal space
+    const decodeURL = decodeURIComponent(req.url);
+    const filePath = path.join(__dirname, decodeURL);
+
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end("File not found");
+        return;
+      }
+
+      const ext = path.extname(filePath); // grab file extension
+
+      const mimeTypes = {
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+        ".jpeg": "image/jpeg",
+        ".gif": "image/gif",
+        ".svg": "image/svg+xml",
+      }; // a lookup for the browser
+
+      res.setHeader(
+        "Content-Type",
+        mimeTypes[ext] || "application/octet-stream",
+      );
+
+      res.end(content);
+    });
+
+    return;
+  }
   // initialize file path
   let filePath;
 
@@ -20,7 +53,7 @@ const server = http.createServer((req, res) => {
     filePath = path.join("pages", "contact-me.html");
   } else {
     filePath = path.join("pages", "404.html");
-    res.statusCode = 400;
+    res.statusCode = 404;
   }
 
   // Read and send content
@@ -28,7 +61,7 @@ const server = http.createServer((req, res) => {
     // handle error
     if (err) {
       res.statusCode = 500;
-      res.end("Internal server erro");
+      res.end("Internal server error");
       return;
     }
     // respond with content
